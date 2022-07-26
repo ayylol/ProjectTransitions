01f7b6f0df63c144e6174b0002f1036ab3b1e0ef
David Garcia Orozco <David.GarOro@protonmail.com>
2022-07-25 18:19:48 -0600

    refactoring hard coded bits

--- quiz_game/RunnerManager.gd
+++ quiz_game/RunnerManager.gd
@@ -3,6 +3,10 @@ extends Node2D
 signal show_question(question, answers)
 signal chose_answer(answer)
 
+var score = 0
+var current_question = 0
+var prompts
+var prompt
 var player_lane
 var questions
 
@@ -18,10 +22,10 @@ func _unhandled_input(event):
 	elif event.is_action_pressed("test3"):
 		lanes[2].spawn()
 	elif event.is_action_pressed("ui_accept"):
-		emit_signal("show_question", "what is 2+2", questions["what is 2+2"]["choices"])
+		show_question()
 	elif event.is_action_pressed("ui_right"): # THIS NEED TO BE MOVED
 		emit_signal("chose_answer", player_lane)
-		if player_lane == questions["what is 2+2"]["answer"]:
+		if player_lane == questions["choose the right answer"]["answer"]:
 			print("correct")
 		else:
 			print("incorrect")
@@ -30,13 +34,25 @@ func _ready():
 	player_lane = int(lanes.size()/2)
 	player.move_to(lanes[player_lane].position, [0.75,1,1.25][player_lane])
 	questions = Database.get_content("res://content/test_quiz.json")
+	prompts = questions.keys()
+
+func show_question():
+	prompt = prompts[current_question]
+	emit_signal("show_question", prompt, questions[prompt]["choices"])
+	current_question +=1
+
+func answer_question():
+	emit_signal(prompt, player_lane)
+	if player_lane == questions[prompt]["answer"]:
+		print("correct")
+	else:
+		print("incorrect")
 
 func _on_Player_moving(direction):
 	var next_lane = player_lane + direction
 	if next_lane >=0 and next_lane <lanes.size():
 		player_lane = next_lane
 	player.move_to(lanes[player_lane].position, [0.75,1,1.25][player_lane])
-
 
 func _on_ObstacleTrash_area_entered(area):
 	if area.is_in_group("Trashable"):
