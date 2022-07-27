ce6ec2af1020ed2fb4c5ca6769d0e210f0c8e6d6
David Garcia <David.GarOro@protonmail.com>
2022-07-26 21:04:55 -0600

    automatic obstacle spawning

--- quiz_game/Lanes.gd
+++ quiz_game/Lanes.gd
@@ -1,19 +1,54 @@
 extends Node2D
 
+# WOULD BE MORE ROBUST IF THIS WAS DONE THROUGH CODE
+var patterns = [
+	[false,false,false],
+	[true,false,false],
+	[false,true,false],
+	[false,false,true],
+	[true,false,true],
+	[true,true,false],
+	[false,true,true],
+]
+
+var _last_pattern = 0
+
 onready var label = $Label
+onready var obstacle_timer = $ObstacleTimer
+onready var lanes = get_tree().get_nodes_in_group("Lane")
 
 func _on_Runner_show_question(question, answers):
 	label.show()
 	label.bbcode_text = question
 	var i = 0
-	for lane in get_children():
-		if lane.is_in_group("Lane"):
-			lane.show_text(answers[i])
-			i+=1
+	for lane in lanes:
+		lane.show_text(answers[i])
+		i+=1
 
 
 func _on_Runner_chose_answer(answer):
 	label.hide()
-	for lane in get_children():
-		if lane.is_in_group("Lane"):
-			lane.hide_text()
+	for lane in lanes:
+		lane.hide_text()
+
+func spawn_obstacles():
+#	var choices = range(patterns.size())
+#	var i = 0
+#	print(choices)
+#	while i < choices.size():
+#		var valid_next = false
+#		for j in range(patterns[_last_pattern].size()):
+#			if (not patterns[i][j]) and (not patterns[_last_pattern][j]):
+#				i+=1
+#				valid_next = true
+#				break
+#		if valid_next: continue
+#		choices.pop_at(i)
+#	var choice = choices[randi()%choices.size()]
+#	_last_pattern = choice
+	var pattern = patterns[randi()%patterns.size()]
+	var i = 0
+	for lane in lanes:
+		if pattern[i]:
+			lane.spawn()
+		i+=1
--- quiz_game/RunnerManager.gd
+++ quiz_game/RunnerManager.gd
@@ -11,24 +11,17 @@ var player_lane
 var questions
 
 onready var lanes = get_tree().get_nodes_in_group("Lane")
+onready var lane_parent = $Lanes
 onready var player = $Player
 
 # TO TEST SPAWNING
 func _unhandled_input(event):
 	if event.is_action_pressed("test1"):
-		lanes[0].spawn()
-	elif event.is_action_pressed("test2"):
-		lanes[1].spawn()
-	elif event.is_action_pressed("test3"):
-		lanes[2].spawn()
+		lane_parent.spawn_obstacles()
 	elif event.is_action_pressed("ui_accept"):
 		show_question()
 	elif event.is_action_pressed("ui_right"): # THIS NEED TO BE MOVED
-		emit_signal("chose_answer", player_lane)
-		if player_lane == questions["choose the right answer"]["answer"]:
-			print("correct")
-		else:
-			print("incorrect")
+		answer_question()
 
 func _ready():
 	player_lane = int(lanes.size()/2)
@@ -37,20 +30,24 @@ func _ready():
 	prompts = questions.keys()
 
 func show_question():
-	prompt = prompts[current_question]
-	emit_signal("show_question", prompt, questions[prompt]["choices"])
-	current_question +=1
+	if current_question < prompts.size():
+		prompt = prompts[current_question]
+		emit_signal("show_question", prompt, questions[prompt]["choices"])
+		current_question +=1
+	else:
+		print("Game is done")
 
 func answer_question():
-	emit_signal(prompt, player_lane)
-	if player_lane == questions[prompt]["answer"]:
-		print("correct")
-	else:
-		print("incorrect")
+	if prompt != null:
+		emit_signal("chose_answer", player_lane)
+		if player_lane == questions[prompt]["answer"]:
+			print("correct")
+		else:
+			print("incorrect")
 
 func _on_Player_moving(direction):
 	var next_lane = player_lane + direction
-	if next_lane >=0 and next_lane <lanes.size():
+	if next_lane >= 0 and next_lane < lanes.size():
 		player_lane = next_lane
 	player.move_to(lanes[player_lane].position, [0.75,1,1.25][player_lane])
 
